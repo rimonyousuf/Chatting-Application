3cd1cee23c42e6ed006a0043531352a5f10dbf7c
Rimon <rimonyousuf00@gmail.com>
2024-08-07 23:05:35 +0600

    Socket io has been created

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,8 +2,15 @@ import { Paper } from "@mui/material";
 import Sidebar from "../../Shared/Sidebar/Sidebar";
 import ChatBox from "../../Shared/ChatBox/ChatBox";
 import Profile from "../../Shared/Profile/Profile";
+import { useEffect } from "react";
+import socket from "../../socket/socket";
 
 const Chat = () => {
+
+    useEffect(()=>{
+        console.log(socket);
+    },[socket])
+
     return (
         <Paper square elevation={0} sx={{height: "100vh", display: "flex"}}>
             <Sidebar/>
---
+++ src/socket/socket.js
@@ -0,0 +1,4 @@
+import io from "socket.io-client";
+const url = "http://localhost:5000";
+const socket = io(url);
+export default socket;
